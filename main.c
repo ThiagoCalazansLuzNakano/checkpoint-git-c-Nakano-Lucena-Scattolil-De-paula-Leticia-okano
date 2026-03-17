0b57d7c2b70e13bd45b97b1e12b38291cb39983f
leticiaokano <rm571988@fiap.com.br>
2026-03-17 12:03:25 -0300

    Adicionado a idade- Leticia okano

--- main.c
+++ main.c
@@ -4,12 +4,9 @@ int main(){
     printf("Thiago Calazans Luz Nakano\n");
     printf("Guilherme De Lucena Fontes\n");
     printf("Gabriel de Paula Santos\n");
-    printf("Leticia Aiko Okano"\n);
-    printf("Enzo Scattolin Furtado\n");
-    return 0;
-}
 
-int main(){ 
-  int (idade);
-  printf("20 anos"); 
+    printf("Leticia Aiko Okano, 18 anos\n");
+    printf("Enzo Scattolin Furtado\n");
+    
+    return 0;
 }
