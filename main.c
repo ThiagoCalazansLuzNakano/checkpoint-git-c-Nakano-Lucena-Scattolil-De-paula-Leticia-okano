81e7ccdb7bc68d251c9fd8900cd301492870ab32
gab-dpaula <depaulasantos.gabriel2007@gmail.com>
2026-03-17 16:48:17 -0300

    adicionado time do coração do gabriel

--- main.c
+++ main.c
@@ -3,7 +3,7 @@
 int main(){
     printf("Thiago Calazans Luz Nakano, 17 anos\n");
     printf("Guilherme De Lucena Fontes 20 anos\n");
-    printf("Gabriel de Paula Santos, 18 anos\n");
+    printf("Gabriel de Paula Santos, 18 anos, meu time do coração é o São Paulo!\n");
 
     printf("Leticia Aiko Okano, 18 anos\n");
     printf("Enzo Scattolin Furtado\n");
