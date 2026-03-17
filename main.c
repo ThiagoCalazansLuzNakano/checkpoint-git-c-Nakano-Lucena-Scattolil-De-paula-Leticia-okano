e4c4cb97e61a8cb82adcd806a02dfff252a592e2
leticiaokano <rm571988@fiap.com.br>
2026-03-17 11:39:54 -0300

    Adicionado o nome - leticia okano

--- main.c
+++ main.c
@@ -4,6 +4,7 @@ int main(){
     printf("Thiago Calazans Luz Nakano\n");
     printf("Guilherme De Lucena Fontes\n");
     printf("Gabriel de Paula Santos\n");
+    printf("Leticia Aiko Okano"\n);
 
     return 0;
 }
