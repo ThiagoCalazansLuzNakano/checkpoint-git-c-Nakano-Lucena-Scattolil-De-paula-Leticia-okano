bb082cb5cb0227731d9ebdaecec6e608954affe3
enzoscattolin <enzoscatolini@gmail.com>
2026-03-17 22:29:05 -0300

    add idade enzo

--- main.c
+++ main.c
@@ -6,7 +6,7 @@ int main(){
     printf("Gabriel de Paula Santos, 18 anos, meu time do coração é o São Paulo!\n");
 
     printf("Leticia Aiko Okano, 18 anos, meu time do coração é o São Paulo\n");
-    printf("Enzo Scattolin Furtado\n");
+    printf("Enzo Scattolin Furtado, 18 anos\n");
     
     return 0;
 }
